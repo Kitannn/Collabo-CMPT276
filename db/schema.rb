7cfcc221ceebdfe636a91e357e80cd80ce631ca1
Bryce <brycegolamco@gmail.com>
2017-06-30 10:22:05 +0000

    Articles currently tied to user ID. Must change later

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630055155) do
+ActiveRecord::Schema.define(version: 20170630101427) do
+
+  create_table "articles", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
